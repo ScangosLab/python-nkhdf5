9a33708c25e515f8b5a9c590adb6a5257ef1e0d5
Ankit Khambhati <akhambhati@gmail.com>
2023-07-27 05:55:45 -0700

    Add header template file

---
+++ src/nkhdf5/header.py
@@ -0,0 +1,28 @@
+"""header.py
+Information about NKHDF5.
+"""
+# Header #
+__package_name__ = "nkhdf5"
+
+__author__ = "Ankit Khambhati"
+__credits__ = ["Akit Khambhati"]
+__maintainer__ = "Ankit Khambhati"
+__email__ = ""
+
+__copyright__ = "Copyright 2023, Ankit Khambhati"
+__license__ = "Apache 2.0"
+
+__version__ = "2023.07.20"
+__status__ = "Planning"
+
+__all__ = [
+    "__package_name__",
+    "__author__",
+    "__credits__",
+    "__maintainer__",
+    "__email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__status__",
+]
